308936ba452bccc6af97c4131b93d6a53a9a4ba5
H-Ramalan <talktoramalan@gmail.com>
2023-07-13 22:23:35 -0700

    Add quote using external api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Calculator from './components/Calculator';
+import Quote from './components/Quote';
 
 const App = () => (
   <div>
     <Calculator />
+    <Quote />
   </div>
 );
 export default App;
